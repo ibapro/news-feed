1358d9ef66f1d6a0987319c6f4dbd37762af1e6f
mirzayev.mirzayev <mirzayev.mirzayev@gmail.com>
2020-06-27 21:25:06 +0400

    --structure

---
+++ src/main/java/app/controller/TestController.java
@@ -0,0 +1,16 @@
+package app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @ResponseBody
+    @RequestMapping("/spring")
+    public String handle(){
+        return "Spring";
+    }
+
+}
