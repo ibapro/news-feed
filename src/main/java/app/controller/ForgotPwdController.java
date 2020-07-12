4b64756202e177ff71e92be92a5c57ee620e7412
mirzayev.mirzayev <mirzayev.mirzayev@gmail.com>
2020-07-13 03:25:04 +0400

    --forgot pwd/use email

--- src/main/java/app/controller/ForgotPwdController.java
+++ src/main/java/app/controller/ForgotPwdController.java
@@ -1,14 +1,36 @@
 package app.controller;
 
+import app.service.EmailServiceImpl;
+import app.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ForgotPwdController {
 
-    @RequestMapping("/forgot-password")
-    public String handle(){
+    private final EmailServiceImpl emailService;
+    private final UserService userService;
+
+    public ForgotPwdController(EmailServiceImpl emailService, UserService userService) {
+        this.emailService = emailService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/forgot-password")
+    public String handle() {
         return "forgot-password";
     }
 
+    @PostMapping("/forgot-password")
+    public String handlePost(@ModelAttribute(value = "email") String email) {
+        if (!userService.isRegistered(email)) {
+           return "redirect:/forgot-password?emailError";
+        }
+        emailService.sendEmail(email, "Reset Password", userService.changePassword(email));
+        return "index";
+    }
+
 }
+
