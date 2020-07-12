098de2af30c0268c009c017348c71d51590ad485
jalil <celil.ahmad@gmail.com>
2020-07-12 16:34:51 +0400

    -- news controller -- news service -- thymleaf

--- src/main/java/app/controller/MainController.java
+++ src/main/java/app/controller/MainController.java
@@ -1,14 +1,43 @@
 package app.controller;
 
+import app.restclient.response.Articles;
+import app.restclient.response.News;
+import app.service.NewsService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
+@SessionAttributes({ "newsList"})
 public class MainController {
 
-    @RequestMapping("/main-page")
-    public String handle(){
+    private final NewsService newsService;
+
+    public MainController(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
+
+    @GetMapping("/main-page")
+    public String handle(Model model) {
+        News news = newsService.getNews();
+        List<Articles> articles = news.getArticles();
+        articles = newsService.persistAll(articles);
+        model.addAttribute("newsList", articles);
+        return "main-page";
+    }
+
+    @PostMapping("/main-page")
+    public String handlePost(Model model) {
+        News news = newsService.getNews();
+        List<Articles> articles = news.getArticles();
+        articles = newsService.persistAll(articles);
+        model.addAttribute("newsList", articles);
         return "main-page";
     }
 
 }
+
