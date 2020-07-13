bed23f5c913f28a7dd25d0d42a264b0b256276e2
mirzayev.mirzayev <mirzayev.mirzayev@gmail.com>
2020-07-14 00:34:32 +0400

    heroku deploying

--- target/generated-sources/annotations/app/mapper/ArticlesMapperImpl.java
+++ target/generated-sources/annotations/app/mapper/ArticlesMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-07-14T00:13:38+0400",
+    date = "2020-07-14T00:26:48+0400",
     comments = "version: 1.3.0.Beta2, compiler: javac, environment: Java 9.0.1 (Oracle Corporation)"
 )
 @Component
