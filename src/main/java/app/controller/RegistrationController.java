2dd9578b1335c7ec50329730dec2fbf01b288d0d
mirzayev.mirzayev <mirzayev.mirzayev@gmail.com>
2020-07-22 15:09:37 +0400

    --final updates

--- src/main/java/app/controller/RegistrationController.java
+++ src/main/java/app/controller/RegistrationController.java
@@ -2,11 +2,14 @@ package app.controller;
 
 import app.dto.UserDTO;
 import app.exception.UserAlreadyExistException;
+import app.service.EmailServiceImpl;
 import app.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -15,9 +18,11 @@ import javax.validation.Valid;
 public class RegistrationController {
 
     private final UserService userService;
+    private final EmailServiceImpl emailService;
 
-    public RegistrationController(UserService userService) {
+    public RegistrationController(UserService userService, EmailServiceImpl emailService) {
         this.userService = userService;
+        this.emailService = emailService;
     }
 
 
@@ -34,6 +39,7 @@ public class RegistrationController {
         if (userService.isRegistered(user.getEmail()))
             throw new UserAlreadyExistException("There is an account with that email address:" + user.getEmail());
         userService.persist(user);
+        emailService.sendEmail(user.getEmail(),"Confirmation", "You have successfully registered");
         return new ModelAndView("successRegister", "successRegister", user);
     }
 
